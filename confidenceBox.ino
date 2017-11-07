9ad5ea099435b5cb5dd6db1af4de3a55c86f7f0b
chrisdats <christopher.datsikas@gmail.com>
2017-11-07 11:30:05 -0600

    servo functionality added

--- confidenceBox.ino
+++ confidenceBox.ino
@@ -1,23 +1,24 @@
 /* confidenceBox.ino
-Christopher Datsikas
-Assignment 7 DPI - Nov 6 2017
-*/
+ Christopher Datsikas
+ Assignment 7 DPI - Nov 6 2017
+ */
 
 // NeoPixel Ring simple sketch (c) 2013 Shae Erisson
 // released under the GPLv3 license to match the rest of the AdaFruit NeoPixel library
 
 #include <Adafruit_NeoPixel.h>
 #include <avr/power.h>
+#include <Servo.h>
+
 
 // Which pins on the Arduino are connected to the levers?
-#define SW1            2
-#define SW2            3
-#define SW3            4
-#define SW4            5
+int leverPins[] = {
+  2,3,4,5};
+// How many levers are connected?
+int leverCount = 4; 
 
 // Which pin on the Arduino is connected to the NeoPixels?
 #define PIN            6
-
 // How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS      4                                                           
 
@@ -26,40 +27,59 @@ Assignment 7 DPI - Nov 6 2017
 // example for more information on possible values.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-int delayval = 500; // delay for half a second
+Servo myservo;  // create servo object to control a servo
+int pos = 0;    // variable to store the servo position
+#define OPEN          90
+#define CLOSE          0
+
 
 void setup() {
   Serial.begin(9600); // Initialize serial communication for debug
-  
+
   pixels.begin(); // This initializes the NeoPixel library.'
-  
+
   // initialize all the neopixels as on, red
-    for(int i=0;i<NUMPIXELS;i++){
+  for(int i=0;i<NUMPIXELS;i++){
     pixels.setPixelColor(i, pixels.Color(25,0,0)); // Moderately bright red color.
     pixels.show(); // This sends the updated pixel color to the hardware.
-    } 
-  
+  } 
+
   // initialize all the pins connected to the lever switches as INPUT with Internal Pull-Up
-  pinMode(SW1, INPUT);
-  digitalWrite(SW1, HIGH);
-  pinMode(SW2, INPUT);
-  digitalWrite(SW2, HIGH);
-  pinMode(SW3, INPUT);
-  digitalWrite(SW3, HIGH);
-  pinMode(SW4, INPUT);
-  digitalWrite(SW4, HIGH);
+  for (int thisPin = 0; thisPin < leverCount; thisPin++) {  
+    pinMode(leverPins[thisPin], INPUT);
+    digitalWrite(leverPins[thisPin], HIGH);
+  }
   
+  // initialize the servo
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.write(OPEN);
+  Serial.println("Servo attached and set to 90");
+  
+  // lock the box
+  for (pos = OPEN; pos >= 1; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    Serial.println(pos);
+    delay(10);                       // waits 15ms for the servo to reach the position
+  }
+  myservo.detach();  // attaches the servo on pin 9 to the servo object
+  delay(3000);
   Serial.println("Setup Complete");
 }
 
 void loop() {
-  int lever1 = digitalRead(SW1);
-  if (lever1 == 0) {
-      pixels.setPixelColor(0, pixels.Color(0,25,0)); // Moderately bright green color.
-      pixels.show(); // This sends the updated pixel color to the hardware.
-  }
-  else if (lever1 == 1) {
-      pixels.setPixelColor(0, pixels.Color(25,0,0)); // Moderately bright red color.
-      pixels.show(); // This sends the updated pixel color to the hardware.
+
+  for (int thisPin = 0; thisPin < leverCount; thisPin++) {    // iterate through all the levers
+    int leverState = digitalRead(leverPins[thisPin]);         // store value of lever state
+    if (leverState == 0) {                                    // if lever is pulled down, turn green
+      pixels.setPixelColor(thisPin, pixels.Color(0,25,0)); // Moderately bright green color.
+      pixels.show();                                       // Update pixel color.
+    }
+    else if (leverState == 1) {                              // if lever is pulled up (or in original position), stay/turn red
+      pixels.setPixelColor(thisPin, pixels.Color(25,0,0)); // Moderately bright red color.
+      pixels.show();                                       // Update pixel color.
+    }
   }
 }
+
+
+
